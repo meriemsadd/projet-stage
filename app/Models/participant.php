8a52930250480298dd3f64b6573d0f8db3e0466b
safae-bellafkih <safae.bellafkih.2005@gmail.com>
2025-07-07 17:09:26 +0100

    ajout du relation entre event et participant

--- app/Models/participant.php
+++ app/Models/participant.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class participant extends Model
+class Participant extends Model
 {
-    //
+
+    protected $fillable=['nom','prenom','profession','evenement_id'];
+    
+    public function evenement()
+{
+    return $this->belongsTo(Evenement::class);
+}
+
 }
