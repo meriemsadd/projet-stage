872297cf64ae664352887ec8fa11482db81447bd
safae-bellafkih <safae.bellafkih.2005@gmail.com>
2025-07-09 15:05:52 +0100

    update des fcts

--- app/Http/Controllers/EvenementController.php
+++ app/Http/Controllers/EvenementController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 use App\Models\Evenement;
 use Illuminate\Http\Request;
+use App\Models\Type;
+
 
 class EvenementController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
          $evenements = Evenement::all(); // Récupère tous les événements du model evenement (base doonnee) 
@@ -17,51 +17,81 @@ class EvenementController extends Controller
          //cad sift l view (evenement.index) wsift m3ah les donne evenements bach nst3mlohom fl view
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
-        //
+       $types=Type::all();
+       return view('evenements.create',compact('types'));
+       
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function store(Request $request)
     {
-        //
+        $request->validate([
+              'titre' => 'required|string|max:255',
+              'lieu' => 'required|string|max:255',
+              'date' => 'required|date',
+              'heure'=> 'required|date_format:H:i',
+              'description'=>'required|string',
+              'type_events_id'=> 'required|exists:type_events,id',
+        ]);
+
+        Evenement::create($request->all());
+        return redirect()->route('evenements.index')->with('suces','Evenement ajoute avec succes');
+
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        
+         $Evenement=Evenement::findOrFail($id);
+        
+       return view('evenements.show',compact('Evenement'));
+      
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(string $id)
     {
-        //
+
+         $Evenement=Evenement::findOrFail($id);
+        
+       return view('evenements.edit',compact('Evenement'));
+     
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(Request $request, string $id)
     {
-        //
+       /* $request->validate([
+            'titre' => 'required|string|max:255',
+              'lieu' => 'required|string|max:255',
+              'date' => 'required|date',
+              'heure'=> 'required|date_format:H:i',
+              'categorie'=> 'required|string'
+        ]);*/ 
+
+        $Evenement=Evenement::findOrFail($id);
+        $Evenement->update($request->all());
+        
+         
+        /*$request->update([
+            'titre'=>$request->titre,
+            'lieu'=>$request->lieu,
+            'date'=>$request->date,
+            'heure'=>$request->heure,
+            'categorie'=>$request->categorie,
+        ]);*/
+
+
+        return redirect('/evenements')->with('suces','Evenement modifie avec succes');
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+ 
     public function destroy(string $id)
     {
-        //
+       
     }
 }
