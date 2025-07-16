42e9176ab28a8e9949c7382440d89029d2a0a251
safae-bellafkih <safae.bellafkih.2005@gmail.com>
2025-07-16 14:19:55 +0100

    ajout du route resetlogin

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,13 @@ Route::get('/',[AcceuilController::class,'index'])->name('acceuil');//la racine 
 Route::resource('evenements', EvenementController ::class);
 Route::resource('participants', ParticipantController::class)->except(['create']);//sinn ghiur hado f participant Route::get('/inscription', [ParticipantController::class, 'create']);Route::post('/inscription', [ParticipantController::class, 'store'])->name('participants.store');
 Route::get('/participants/create/{evenementId}', [ParticipantController::class, 'create'])->name('participants.create');
+Route::get('/evenements/{id}/show1', [EvenementController::class, 'show1'])->name('evenements.show1');
+
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/loginReset', [LoginController::class, 'reset'])->name('loginReset');
+Route::get('/loginReset', [LoginController::class, 'showResetForm'])->name('loginReset.form');
+
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
