5a384abd542fc92b34db86a3f7f949b163fc89be
safae-bellafkih <safae.bellafkih.2005@gmail.com>
2025-07-23 11:13:06 +0100

    titre ajuste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8" />
-    <title>@yield('title', 'Wilaya De L'Oriental')</title>
+    <title>@yield("title", "Wilaya De L'Oriental")</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
     <style>
